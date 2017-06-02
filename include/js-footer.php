da66d3cb35fa216d6db6669ca4fe47225d052ed6
ouoam <poompathai.cha@pccpl.ac.th>
2017-06-02 17:20:06 +0700

    fix copy paste error

--- include/js-footer.php
+++ include/js-footer.php
@@ -1,19 +1,7 @@
+<script type="text/javascript" src="assets/bootstrap-inputmask/bootstrap-inputmask.min.js"></script>
 
+<!--common script for all pages-->
+<script src="js/common-scripts.js"></script>
 
-
-
-
-  
- 
-
-
-
-  <script type="text/javascript" src="assets/bootstrap-inputmask/bootstrap-inputmask.min.js"></script>
-
-
-
-  <!--common script for all pages-->
-    <script src="js/common-scripts.js"></script>
-
-  <!--script for this page-->
-  <script src="js/form-component.js"></script>
+<!--script for this page-->
+<script src="js/form-component.js"></script>
--- include/nav-hd.php
+++ include/nav-hd.php
@@ -1,25 +1,27 @@
-      <header class="header white-bg">
-              <div class="sidebar-toggle-box">
-                  <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
-              </div>
-            <!--logo start-->
-            <a href="index.php" class="logo">PCCPL<span> Info</span></a>
-            <!--logo end-->
-			<div class="top-nav ">
-              <ul class="nav pull-right top-menu">
-                  <!-- user login dropdown start-->
-                  <li class="dropdown">
-                      <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                          <img alt="" src="img/user (4).png">
-                          <span class="username">เข้าสู่ระบบ</span>
-                          <b class="caret"></b>
-                      </a>
-                      <ul class="dropdown-menu extended logout">
-                          <div class="log-arrow-up"></div>
-                          <li><a href="login.php"><i class="fa fa-key"></i>LOG IN</a></li>
-                      </ul>
-                  </li>
-                  <!-- user login dropdown end -->
-              </ul>
-          </div>
-        </header>
+        <header class="header white-bg">
+                <div class="sidebar-toggle-box">
+                    <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+                </div>
+                <!--logo start-->
+                <a href="index.php" class="logo">PCCPL<span> Info</span></a>
+                <!--logo end-->
+                <div class="top-nav ">
+                    <ul class="nav pull-right top-menu">
+                        <!-- user login dropdown start-->
+                        <li class="dropdown">
+                            <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                                <img alt="" src="img/user (4).png">
+                                <span class="username">เข้าสู่ระบบ</span>
+                                <b class="caret"></b>
+                            </a>
+                            <ul class="dropdown-menu extended logout">
+                                <div class="log-arrow-up"></div>
+                                <li>
+                                    <a href="login.php"><i class="fa fa-key"></i>LOG IN</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <!-- user login dropdown end -->
+                    </ul>
+                </div>
+            </header>
