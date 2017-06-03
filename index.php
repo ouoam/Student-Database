c1315a5038fd5b40a63da023a1c6dd527b4f9340
ouoam <poompathai.cha@pccpl.ac.th>
2017-06-03 09:31:18 +0700

    edit developer and add TCPDF

--- index.php
+++ index.php
@@ -30,32 +30,34 @@
                                   </div>
 
                                   <div class="timeline">
-                                      <article class="timeline-item alt">
+                                      <article class="alt">
+                                      <!-- <article class="timeline-item alt">  -->
                                           <div class="timeline-desk">
                                               <div class="panel">
                                                   <div class="panel-body">
                                                       <span class="arrow-alt"></span>
                                                       <span class="timeline-icon purple"></span>
-                                                      <span class="timeline-date">18.00 น.</span>
+                                                      <!-- <span class="timeline-date">18.00 น.</span> -->
                                                       <h1 class="purple">25 พฤษภาคม </h1>
                                                       <h5>เริ่มพัฒนาระบบ   </h5>
                                                       <div class="notification">
                                                         <i class=" fa fa-exclamation-sign">Database Design</i><a href="https://fb.com/numnoy.surajit"> -- อาจารย์สุรจิตร นามวิบูรณ์</a><br>
-                                                        <i class=" fa fa-exclamation-sign">Web Design & System Developer</i><a href="https://fb.com/few.raweroj">  -- นายรวีโรจน์ ทองดี</a><br>
-                                                        <i class=" fa fa-exclamation-sign">Front & Back ends Developer</i><a href="https://fb.com/ouoam"> -- นายภูมิไผท จันทรศรีวงศ์</a><br>
+                                                        <i class=" fa fa-exclamation-sign">Web Designer & System Management</i><a href="https://fb.com/few.raweroj">  -- นายรวีโรจน์ ทองดี</a><br>
+                                                        <i class=" fa fa-exclamation-sign">Front & Back Ends Developer</i><a href="https://fb.com/ouoam"> -- นายภูมิไผท จันทรศรีวงศ์</a><br>
                                                         <i class=" fa fa-exclamation-sign">Resource</i><a href="https://fb.com/nominal.minal"> -- อาจารย์พุทธพร อินอ่อน</a>
                                                       </div>
                                                   </div>
                                               </div>
                                           </div>
                                       </article>
-                                      <article class="timeline-item">
+                                      <article class="">
+                                      <!-- <article class="timeline-item"> -->
                                           <div class="timeline-desk">
                                               <div class="panel">
                                                   <div class="panel-body">
                                                       <span class="arrow"></span>
                                                       <span class="timeline-icon red"></span>
-                                                      <span class="timeline-date">08:30 น.</span>
+                                                      <!-- <span class="timeline-date">08:30 น.</span> -->
                                                       <h1 class="red">5 มิถุนายน </h1>
                                                       <h5>ทดลองใช้งานระบบ เปิดให้นักเรียนลงทะเบียน</h5>
                                                   </div>
